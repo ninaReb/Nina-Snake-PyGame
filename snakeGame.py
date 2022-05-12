7f822ccfc30bff285eebd5b4252f7e853ddf3783
ninaReb <nina.reb@gmail.com>
2022-05-12 00:22:02 -0300

    created frame eventes and keyboard direction commands

--- snakeGame.py
+++ snakeGame.py
@@ -46,7 +46,26 @@ def gameOver():
     pygame.quit()
     sys.exit()
     
-    
+# Events
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                changeto = 'RIGHT'
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                changeto = 'LEFT'
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                changeto = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                changeto = 'DOWN'
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+            
+            
+            
 
 
 
